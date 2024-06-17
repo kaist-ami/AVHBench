ab32da85e043a782cc4582a2eea265d39094c26e
AVHBench <hyunbinoh@g.postech.edu>
2024-06-17 15:28:39 +0900

    ram refined

--- stage1_gpt4/recognize-anything/batch_infer.sh
+++
@@ -1,5 +0,0 @@
-python batch_inference.py \
-    --model-type ram_plus \
-    --checkpoint ./pretrained/ram_plus_swin_large_14m.pth \
-    --dataset openimages_common_214 \
-    --output-dir outputs/demo
